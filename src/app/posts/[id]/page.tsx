4042526b5ac990b7ad3a0949fae8197268c456fe
Micael <micaelxv1@gmail.com>
2025-02-18 14:11:09 -0300

    loading

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,4 +1,6 @@
 import { PostProps } from '../page';
+import { PostInfo } from './_components/post';
+import { Suspense } from 'react';
 
 export default async function DetaisPost({
   params
@@ -8,15 +10,22 @@ export default async function DetaisPost({
 
   const { id } = await params;
 
+  await new Promise(resolve => setTimeout(resolve, 4000)) // Simulando uma requisição lenta
+
   const response = await fetch(`https://dummyjson.com/posts/${id}`);
   const data: PostProps = await response.json();
 
   return (
     <div>
-      <h1>Detalhes do Post: {id}</h1>
+      <h1 className='text-4xl font-bold text-center'>
+        Detalhes do Post: {id}
+      </h1>
 
-      <h2>{data.title}</h2>
-      <p>{data.body}</p>
+      // Suspense é um componente do React que permite que você suspenda a renderização enquanto aguarda que algo seja carregado.
+      <Suspense fallback={<h1>Carregando...</h1>}> // fallback é o que será exibido enquanto o conteúdo estiver sendo carregado
+        <PostInfo id={id}/> 
+      </Suspense>
+
     </div>
   )
 }
